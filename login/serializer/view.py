b2cf3e027d95ee9a51bf9012526f43fc8eaa04ac
Halep <henryp@estudiantes.uci.cu>
2025-04-04 15:15:47 -0400

    jugando con el CRUD de DRF para aprender

--- login/serializer/view.py
+++ login/serializer/view.py
@@ -1,6 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import HttpResponse
+ 
 
-@api_view()
+@api_view(['GET','POST','PUT','DELETE'])
 def hello_world(request):
-    return Response({"message": "Hello, world!"})
+
+    if request.method == 'POST':
+        return HttpResponse("haz creado algo nuevo")
+
+    if request.method == 'PUT':
+        return HttpResponse("Lo acabas de modificar")
+
+    if request.method == 'DELETE':
+        return Response({'message':'eleminado'})
+
+    if request.method == 'GET':
+        return Response({'message': 'Que haces ahora'})
+
+    return Response({'message':'Hola'})
