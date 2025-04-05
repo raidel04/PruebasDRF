6104e47c1d45aadae4adc25b4d691a4858578874
Halep <henryp@estudiantes.uci.cu>
2025-04-05 12:08:07 -0400

    acabo de crear mi primera vista basada en clases

--- login/serializer/view.py
+++ login/serializer/view.py
@@ -1,21 +1,16 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import HttpResponse
  
-
-@api_view(['GET','POST','PUT','DELETE'])
-def hello_world(request):
-
-    if request.method == 'POST':
-        return HttpResponse("haz creado algo nuevo")
-
-    if request.method == 'PUT':
-        return HttpResponse("Lo acabas de modificar")
-
-    if request.method == 'DELETE':
-        return Response({'message':'eleminado'})
-
-    if request.method == 'GET':
-        return Response({'message': 'Que haces ahora'})
-
-    return Response({'message':'Hola'})
+class MyAppi(APIView):
+    def get(self,request,pk= None):
+        return Response({'message':'hola como estas'})
+    
+    def post(self,request,pk = None):
+        return HttpResponse("creado")
+    
+    def put(self,request,pk= None):
+        return HttpResponse('modificado')
+    
+    def delete(self,request,pk = None):
+        return HttpResponse('delete')
--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from login.serializer import view
+from login.serializer.view import MyAppi
+from rest_framework.routers import DefaultRouter
+
 
 urlpatterns = [
-    path("", view.hello_world, name = ""),
-    ]
+    path("", MyAppi.as_view(), name="lista"),  # Sin PK (GET, POST)
+    path("<int:pk>/", MyAppi.as_view(), name="detalle"),  # Con PK (GET, PUT, DELETE)
+]
